c7c3b6ecc566fb6c9f2bb5d04cabbd35817d6548
Rufus Wong <rcywongaa@gmail.com>
2019-09-26 17:05:54 +0800

    Use equation of motion Use coefficient of friction to constrain x direction force

--- stair_climb_traj_opt.py
+++ stair_climb_traj_opt.py
@@ -6,6 +6,9 @@ import numpy as np
 import math
 from enum import Enum
 import time
+import pdb
+
+import eom
 
 class Method(Enum):
     DIRECT_TRANSCRIPTION = 1
@@ -30,6 +33,25 @@ w_r = 0.1 # wheel radius
 I_w = 1.0/2.0*m_w*(w_r**2)
 NUM_TIME_STEPS = 500
 TIME_INTERVAL = 0.001
+COEFF_FRICTION = 0.6
+
+def calc_theta1_dd(state, u):
+    return eom.calc_theta1_dd(
+            state[0], state[1], state[2],
+            state[4], state[5], state[6],
+            u[0], u[1], u[2], u[3]*w_r)
+
+def calc_theta2_dd(state, u):
+    return eom.calc_theta2_dd(
+            state[0], state[1], state[2],
+            state[4], state[5], state[6],
+            u[0], u[1], u[2], u[3]*w_r)
+
+def calc_theta3_dd(state, u):
+    return eom.calc_theta3_dd(
+            state[0], state[1], state[2],
+            state[4], state[5], state[6],
+            u[0], u[1], u[2], u[3]*w_r)
 
 def findTheta1(theta2, theta3, theta4):
     y = theta4*w_r
@@ -121,6 +143,9 @@ def find_I_wrt_3():
     return m_w*(l_3**2)
 
 def derivs(state, tau234):
+    tau2 = tau234[0]
+    tau3 = tau234[1]
+    tau4 = tau234[2]
     theta1 = state[0]
     theta2 = state[1]
     theta3 = state[2]
@@ -137,35 +162,63 @@ def derivs(state, tau234):
     c3 = cos(theta3)
     c12 = cos(theta12)
     c123 = cos(theta123)
+
+    tau1 = findTau1(theta1, theta2, theta3)
+    tau = np.array([tau1, tau2, tau3, tau4])
+    state_d = np.zeros_like(state)
+    state_d[0:4] = state[4:8]
+    state_d[4] = calc_theta1_dd(state, tau)
+    state_d[5] = calc_theta2_dd(state, tau)
+    state_d[6] = calc_theta3_dd(state, tau)
+    state_d[7] = tau234[2] / I_w
+    return state_d
+
+def findJacobian(theta1, theta2, theta3):
+    theta12 = theta2 + theta1
+    theta123 = theta3 + theta12
+    s1 = sin(theta1)
+    s2 = sin(theta2)
+    s3 = sin(theta3)
+    s12 = sin(theta12)
+    s123 = sin(theta123)
+    c1 = cos(theta1)
+    c2 = cos(theta2)
+    c3 = cos(theta3)
+    c12 = cos(theta12)
+    c123 = cos(theta123)
     J = np.array([
         [-l_1*s1 - l_2*s12 - l_3*s123, -l_2*s12 - l_3*s123, -l_3*s123],
         [l_1*c1 + l_2*c12 + l_3*c123, l_2*c12 + l_3*c123, l_3*c123]])
-
-    tau1 = findTau1(theta1, theta2, theta3)
-    state_d = np.zeros_like(state)
-    state_d[0:4] = state[4:8]
-    state_d[4] = tau1 / find_I_wrt_1(theta1, theta2, theta3)
-    state_d[5] = tau234[0] / find_I_wrt_2(theta2, theta3)
-    state_d[6] = tau234[1] / find_I_wrt_3()
-    state_d[7] = tau234[2] / I_w
-    return state_d
+    return J
 
 if __name__ == "__main__":
     x = 0.92
     mp = MathematicalProgram()
     state_over_time = np.zeros(shape=(NUM_TIME_STEPS, 8), dtype=pydrake.symbolic.Expression)
+
     if method == Method.SHOOTING:
         initial_state = np.array([1.57, 1.57, 1.57, 0.0, 0.0, 0.0, 0.0, 0.0])
         state_over_time[0] = initial_state
     elif method == Method.DIRECT_TRANSCRIPTION:
         state_over_time[0] = mp.NewContinuousVariables(8, "state_0")
+
     tau234_over_time = np.zeros(shape=(NUM_TIME_STEPS, 3), dtype=pydrake.symbolic.Variable)
     for i in range(NUM_TIME_STEPS-1):
-        tau234_over_time[i] = mp.NewContinuousVariables(3, "tau234_%d" % i)
+        print("Adding constraints for t = " + str(i))
+        tau234 = mp.NewContinuousVariables(3, "tau234_%d" % i)
+        tau234_over_time[i] = tau234
 
         if method == Method.DIRECT_TRANSCRIPTION:
             state_over_time[i+1] = mp.NewContinuousVariables(8, "state_%d" % (i+1))
-            # pdb.set_trace()
+
+            theta1 = state_over_time[i+1][0]
+            theta2 = state_over_time[i+1][1]
+            theta3 = state_over_time[i+1][2]
+            tau1 = findTau1(theta1, theta2, theta3)
+            J = findJacobian(theta1, theta2, theta3)
+            tau123 = np.array([tau1, tau234[0], tau234[2]])
+            mp.AddConstraint(COEFF_FRICTION*J.dot(tau123)[0] <= -w_r*tau234[2])
+
             for j in range(3): # Constrain theta1, theta2, theta3
                 mp.AddConstraint(state_over_time[i+1][j] >= 0.0)
                 mp.AddConstraint(state_over_time[i+1][j] <= np.pi)
@@ -179,12 +232,23 @@ if __name__ == "__main__":
     mp.AddCost(0.01 * tau234_over_time[:,0].dot(tau234_over_time[:,0]))
     mp.AddCost(0.01 * tau234_over_time[:,1].dot(tau234_over_time[:,1]))
     mp.AddCost(0.01 * tau234_over_time[:,2].dot(tau234_over_time[:,2]))
-    mp.AddCost(state_over_time[:,0].dot(state_over_time[:,0]))
+    mp.AddCost(-(state_over_time[:,0].dot(state_over_time[:,0])))
     target_theta4 = STAIR_HEIGHT / w_r
-    mp.AddConstraint(state_over_time[0, 2] <= np.pi/2.0)
-    mp.AddConstraint(state_over_time[0, 2] >= 0.0)
+
+    # Constraint initial and final velocity to be 0
+    for j in range(4, 8):
+        mp.AddConstraint(state_over_time[0, j] <= 0.0)
+        mp.AddConstraint(state_over_time[0, j] >= 0.0)
+        mp.AddConstraint(state_over_time[-1, j] <= 0.0)
+        mp.AddConstraint(state_over_time[-1, j] >= 0.0)
+
+    for j in range(0, 4):
+        mp.AddConstraint(state_over_time[0, j] >= 0.0)
+        mp.AddConstraint(state_over_time[0, j] <= np.pi)
+
     mp.AddConstraint(state_over_time[0, 3] <= 0.0)
     mp.AddConstraint(state_over_time[0, 3] >= 0.0)
+
     mp.AddConstraint(state_over_time[-1, 3] <= target_theta4)
     mp.AddConstraint(state_over_time[-1, 3] >= target_theta4)
 
@@ -194,4 +258,6 @@ if __name__ == "__main__":
     print("Done solving in " + str(time.time() - t) + "s")
     is_success = result.is_success()
     print("is_success = " + str(is_success))
-    torques_over_time = result.GetSolution(tau234_over_time)
+    torque_over_time = result.GetSolution(tau234_over_time)
+    state_over_time = result.GetSolution(state_over_time)
+    pdb.set_trace()
