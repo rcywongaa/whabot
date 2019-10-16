d666d59cfa8b2a1daa9103579c6a8e16b91ccbaf
Rufus Wong <rcywongaa@gmail.com>
2019-10-16 23:38:50 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install python3-dill python3-sympy
+
+# See
+# https://drake.mit.edu/from_binary.html
+# https://drake.mit.edu/python_bindings.html#python-bindings-binary
+wget https://drake-packages.csail.mit.edu/drake/nightly/drake-latest-bionic.tar.gz
+sudo tar -xvzf drake-latest-bionic.tar.gz -C /opt
+sudo /opt/drake/share/drake/setup/install_prereqs
