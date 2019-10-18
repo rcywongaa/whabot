345f07a2246662586389062b634e339592db35be
Rufus Wong <rcywongaa@gmail.com>
2019-10-19 01:23:46 +0800

    Add drake path in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ sudo apt install python3-dill python3-sympy
 wget https://drake-packages.csail.mit.edu/drake/nightly/drake-latest-bionic.tar.gz
 sudo tar -xvzf drake-latest-bionic.tar.gz -C /opt
 sudo /opt/drake/share/drake/setup/install_prereqs
+
+echo "export PYTHONPATH=/opt/drake/lib/python3.6/site-packages:${PYTHONPATH}" >> ~/.bashrc
